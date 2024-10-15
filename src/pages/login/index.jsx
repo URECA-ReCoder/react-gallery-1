8ddd660f520bccc7a905a59b62d225dfde15a9fb
sieon <sieon.eve@gmail.com>
2024-10-15 20:38:28 +0900

    ✨ Feat: LoginOptionPage 완성

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,22 +1,31 @@
-import { Footer } from "../../components/common/commonUI";
-import { ButtonStyled,LogoContainer, NoLogin,CenterItems } from "./index.styles";
+import { Footer } from '../../components/common/commonUI';
+import {
+  WrapperStyle,
+  ButtonStyled,
+  LogoContainer,
+  NoLogin,
+  CenterItems,
+  NewIdBtn,
+} from './index.styles';
 import instagram from '../../assets/icons/instagram.png';
 import user from '../../assets/icons/user.png';
 const Login = () => {
-    return (
-        <WrapperStyle>
-            <CenterItems>
-        <LogoContainer >
-            <img src={instagram}/>
+  return (
+    <WrapperStyle>
+      <CenterItems>
+        <LogoContainer>
+          <img src={instagram} />
         </LogoContainer>
         <NoLogin>
-            <img src={user}/>
-            <div style={{color:'black'}}>로그인 없이 이용하기</div>
+          <img src={user} />
+          <div style={{ color: 'black' }}>로그인 없이 이용하기</div>
         </NoLogin>
         <ButtonStyled>다른 계정으로 로그인</ButtonStyled>
-            </CenterItems>
-        <Footer/>
-        </WrapperStyle>
-    );
+      </CenterItems>
+      <Footer>
+        <NewIdBtn>새 계정 만들기</NewIdBtn>
+      </Footer>
+    </WrapperStyle>
+  );
 };
 export default Login;
--- src/pages/login/index.styles.js
+++ src/pages/login/index.styles.js
@@ -1,58 +1,72 @@
 import styled from "styled-components";
 import { Wrapper } from "../../components/common/commonUI";
+
 export const ButtonStyled = styled.div`
-    width: 90%;
-    border-radius: 60px;
-    margin: auto;
-    text-align: center;
-    padding: 7px 0px;
-    border: 1px solid #CBD2D9;
-    color: #000;
-    font-size: 14px;
-`;
+  width: 90%;
+  border-radius: 60px;
+  text-align: center;
+  padding: 10px 0px;
+  border: 1.5px solid #CBD2D9;
+  color: #000;
+  font-size: 14px;
+  cursor: pointer;
+  margin-bottom: 250px;
+  font-weight: 600;
+  `;
+export const NewIdBtn = styled(ButtonStyled)`
+  border: 1.5px solid #0064E0;
+  color: #0064E0;
+  margin: 0 auto;
+  margin-bottom: 0;
+`
+
 export const LogoContainer = styled.div`
-    width: 100%;
-    display: flex;  /* Flexbox로 변경 */
-    justify-content: center;  /* 가로 정렬 */
-    align-items: center;  /* 세로 정렬 */
-    img {
-        margin: auto;
-        width: 100px;
-    }
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    margin: auto;
+    width: 65px;
+    margin-bottom: 60px;
+  }
 `;
 
 export const NoLogin = styled.div`
-    width: 90%;
-    margin: auto;
-    padding: 10px 15px;
-    display: flex;
-    background-color: #fff;
-    align-items: center;
-    border-radius: 10px;
-    margin-bottom: 20px;
-    font-size: 14px;
-    font-weight: 600;
-    img{
-        width: 60px;
-        margin-right: 20px;
-    }
+  width: 90%;
+  padding: 7px 15px;
+  display: flex;
+  background-color: #ffffff;
+  align-items: center;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  font-size: 12px;
+  font-weight: 600;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.05); 
+  cursor: pointer;
+  img {
+    width: 60px;
+    margin-right: 20px;
+  }
 `;
 
 export const CenterItems = styled.div`
-    display: flex;
-    flex-direction: column;  /* 수직 정렬 */
-    justify-content: center;  /* 수직 중앙 정렬 */
-    align-items: center;  /* 가로 중앙 정렬 */
-    width: 100%;  /* 너비를 전체로 설정 */
-    height: 100%;  /* 높이도 전체로 설정 */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;  /* 수직 중앙 정렬 */
+  align-items: center;  /* 가로 중앙 정렬 */
+  width: 100%;  /* 너비를 전체로 설정 */
+  height: 100%; /* 높이도 전체로 설정 */
+  gap: 0; /* 요소 간 간격을 없앰 */
 `;
 
 export const WrapperStyle = styled(Wrapper)`
-    display: flex;
-    flex-direction: column;
-    background: rgb(254,247,244);
-background: linear-gradient(122deg, rgba(254,247,244,1) 0%, rgba(247,241,250,1) 22%, rgba(238,245,254,1) 50%, rgba(236,247,249,1) 77%, rgba(237,251,243,1) 100%);
-justify-content: center;
-align-items: center;  /* 가로 중앙 정렬 */
-/* margin: auto; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;  /* 세로 중앙 정렬 */
+  align-items: center;  /* 가로 중앙 정렬 */
+  height: 100vh;  /* 화면 전체 높이 사용 */
+  max-height: 859px;
+  background: rgb(254,247,244);
+  background: linear-gradient(100deg, rgba(254,247,244,1) 0%, rgba(247,241,250,1) 12%, rgba(238,245,254,1) 54%, rgba(236,247,249,1) 77%, rgba(237,251,243,1) 100%);
 `;
