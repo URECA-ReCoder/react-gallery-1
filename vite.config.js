7172dd8494727feed9ea60947fd9de13e5c327e3
sieon <sieon.eve@gmail.com>
2024-10-15 15:27:45 +0900

    feat: 기본세팅 및 폴더 생성

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@src', replacement: '/src' },
+      {
+        find: '@components',
+        replacement: '/src/components',
+      },
+    ],
+  },
 })
