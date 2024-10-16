0b08b13c7ecb224399b5b7a35deb6f20cf04a6a8
ongheong <pudding4271@gmail.com>
2024-10-17 01:22:27 +0900

    💄 SearchBar UI 구현, search 페이지 상태 화면 추가, usePageStore 전역 상태 정의

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -2,28 +2,37 @@ import { Wrapper } from '@components/common/commonUI';
 import Header from '@components/common/main/header';
 import Footer from '@components/common/main/footer';
 import Collection from '@components/collection';
-// import SearchBar from '@components/searchBar';
+import SearchBar from '@components/searchBar';
 import { useMissions } from '@src/api/MissionAPI';
+import { useSearchStore } from '@src/store/mission';
+import { WaitWrapper } from '@components/common/commonUI';
 
 const Search = () => {
   const { data: missions, isLoading, error } = useMissions();
   const { searchQuery, setSearchQuery } = useSearchStore();
 
-  if (isLoading) return <div>미션 검색중...</div>;
-  if (error) return <div>원하는 미션을 찾지 못했어요!</div>;
-
   // 작성자 혹은 주제로 미션을 필터링함
-  const filteredMissions = missions.filter(
-    (mission) =>
-      mission.creator.includes(searchQuery) ||
-      mission.type.toLowerCase().includes(searchQuery.toLowerCase()),
-  );
+  const filteredMissions = searchQuery
+    ? missions.filter(
+        (mission) =>
+          mission.creator.includes(searchQuery) ||
+          mission.type.toLowerCase().includes(searchQuery.toLowerCase()),
+      )
+    : [];
 
   return (
     <Wrapper>
       <Header />
       <SearchBar />
-      <Collection missions={filteredMissions} />
+      {
+        isLoading ? (
+          <WaitWrapper>로딩 중...</WaitWrapper>
+        ) : error ? (
+          <WaitWrapper>원하는 미션을 찾지 못했어요!</WaitWrapper>
+        ) : (
+          <Collection missions={filteredMissions} />
+        )
+      }
       <Footer></Footer>
     </Wrapper>
   );
